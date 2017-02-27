c3eb07407a0669f1236eff150073c8b5514c278c
musikele <michele.nasti@gmail.com>
2017-02-27 18:54:26 +0100

    encodeURIComponent of address

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+}).help()
+.alias('help', 'h')
+.argv;
+
+console.log(argv);
+
+let encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=san%20biagio%20dei%20librai%20napoli',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
