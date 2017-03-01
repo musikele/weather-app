35f8bbfd0ba21fe35213a03d88a4ec63b51bcfce
musikele <michele.nasti@gmail.com>
2017-03-01 11:19:54 +0100

    refactory of geocode code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const request = require('request');
+
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs.options({
   a: {
@@ -12,25 +14,11 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv;
 
-let encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  
-  if (error) {
-    console.error('Unable to connect to Google Servers');
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
   }
-
-  else if (body.status === 'ZERO_RESULTS') {
-    console.error('Unable to find that address');
+  else {
+    console.log(JSON.stringify(results,undefined, 2));  
   }
-
-  else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  }
-  
 });
